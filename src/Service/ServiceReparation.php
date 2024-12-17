47ae698a31eb6a9afb5596f2bf80d880b8d4540e
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-12-17 15:23:01 +0100

    commit

--- src/Service/ServiceReparation.php
+++ src/Service/ServiceReparation.php
@@ -4,6 +4,7 @@ namespace App\Service;
 class ServiceReparation {
     function connect() {
         $db = parse_ini_file("../../cfg/db_config.ini", true) ["params_db_sql"];
+ 
     }
     function insertReparation() {
 
